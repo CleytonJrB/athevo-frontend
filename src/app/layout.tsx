639be12e782eef5916a0f7e3788bd36db5cef0ef
CleytonB <cleytonjrb@outlook.com>
2026-08-16 11:33:19 -0300

    FEAT: add new provider 'QueryProvider' to 'react-query'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { QueryProvider } from "@/components/providers/query-provider";
 import "./globals.css";
 
 const geist = Geist({
@@ -27,7 +28,9 @@ export default function RootLayout({
       lang="pt-BR"
       className={`${geist.variable} ${geistMono.variable} h-full scroll-smooth antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
